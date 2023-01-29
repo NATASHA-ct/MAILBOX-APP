7c6aaec25ebfedb489f7a4cb1dc792e8e6b440d6
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-01-29 19:28:49 +0200

    Remove mail icon

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { FiMail } from "react-icons/fi";
 import { CSSTransition } from "react-transition-group";
 import "./Home.css";
 
@@ -27,7 +26,7 @@ const Home = () => {
       <div className="homepage page">
         <h1>Hello Person</h1>
         <p className="unreadMsg">
-          <FiMail />You have {unreadMsg || 0} unread messages out of{" "}
+          You have {unreadMsg || 0} unread messages out of{" "}
           {messages?.length || 0}          
         </p>
         <Link to="/Inbox" className="toInbox">
