43961bf7f3910951d0de96fa6f443116d63a9f0e
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-01-30 09:18:35 +0200

    Add signup endpoints and server functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 // login a user
 const loginUser = async (req, res) => {
@@ -7,12 +12,15 @@ const loginUser = async (req, res) => {
 
 // signup a user
 const signupUser = async (req, res) => {
-   const { username, password } = req.body;
+   const { email, password } = req.body;
 
    try {
-     const user = await User.signup(username, password);
+     const user = await User.signup(email, password);
 
-     res.status(200).json({ username, user });
+     
+    const token = createToken(user._id);
+
+     res.status(200).json({ email, token });
    } catch (error) {
      res.status(400).json({ error: error.message });
    }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,7 +5,7 @@ const validator = require("validator");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: {
+  email: {
     type: String,
     required: true,
     unique: true,
@@ -17,29 +17,29 @@ const userSchema = new Schema({
 });
 
 //static signup
-userSchema.statics.signup = async function (username, password) {
+userSchema.statics.signup = async function (email, password) {
 
   // validations
-  if (!username || !password) {
+  if (!email || !password) {
     throw Error("All fields must be filled");
   }
-  if (!validator.isUsername(username)) {
-    throw Error("Username not valid");
+  if (!validator.isEmail(email)) {
+    throw Error("Email not valid");
   }
   if (!validator.isStrongPassword(password)) {
-    throw Error("Password not strong enough");
+    throw Error("Password not strong enough,add at least uppercase, lowercase,alphanumerics");
   }
 
-  const exists = await this.findOne({ username });
+  const exists = await this.findOne({ email });
 
   if (exists) {
-    throw Error("Username already in use!");
+    throw Error("Email already in use!");
   }
 
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
 
-  const user = await this.create({ username, password: hash });
+  const user = await this.create({ email, password: hash });
 
   return user;
 };
