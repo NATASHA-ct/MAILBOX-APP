bd4f75e05573c337000972bbd191bfd7116edf05
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-01-30 18:02:37 +0200

    Set initial log in status

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,11 +2,12 @@ import { FiMail } from "react-icons/fi";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
-
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
 
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   const handleClick = () => {
     logout();
@@ -19,15 +20,19 @@ const Navbar = () => {
         MAILBOX
       </h1>
       <div className="links">
-        <Link to="/">Home</Link>
-        <Link to="/Inbox">Inbox</Link>
-        <div>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
-        </div>
-        <div>
-          <button onClick={handleClick}>Log out</button>
-        </div>
+        {user && (
+          <div>
+            <Link to="/">Home</Link>
+            <Link to="/Inbox">Inbox</Link>
+            <button onClick={handleClick}>Log out</button>
+          </div>
+        )}
+        {!user && (
+          <div>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
+          </div>
+        )}
       </div>
     </nav>
   );
