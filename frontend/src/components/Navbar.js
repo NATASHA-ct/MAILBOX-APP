df28842373a9b7c1d1e3558b7a626985a6e8e76f
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-01-30 17:41:47 +0200

    Add logout hook and functionality

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,17 @@
 import { FiMail } from "react-icons/fi";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 
 
 const Navbar = () => {
+
+  const { logout } = useLogout();
+
+  const handleClick = () => {
+    logout();
+  };
+
   return (
     <nav className="navbar">
       <h1 className="logo">
@@ -14,11 +22,13 @@ const Navbar = () => {
         <Link to="/">Home</Link>
         <Link to="/Inbox">Inbox</Link>
         <div>
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Signup</Link>
-       </div>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+        </div>
+        <div>
+          <button onClick={handleClick}>Log out</button>
+        </div>
       </div>
-      
     </nav>
   );
 };
